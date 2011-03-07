d8cf7d23beea8921e9be1e97d06af0f94618153c
John Cantrell <jdcantrell@gmail.com>
2011-03-06 23:33:37 -0600

    Updated problem 18 with a final solution, will add triangle for problem 67, since this solution should work for much larger triangles (runs on O(n**2) time)

--- euler18.c
+++ euler18.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DIGITS 1000
 
 #define TRUE 1
 #define FALSE 0
 
-#define SIZE 4
+#define SIZE 15
+
 
 typedef struct path_list_struct {
     int sum;
-	int index;
-	int active;
-	int c;
-    unsigned int number[DIGITS];
+    int num;
+	struct path_list_struct *next;
 } path_list;
 
-void update_paths(path_list paths[],int row[]) {
-	int i;
-	for (i = 0; i < SIZE; i++) {
-		if (paths[i].active) {
-			if (paths[i].index == 0 || row[paths[i].index] > row[paths[i].index - 1]) {
-			 	paths[i].sum += row[paths[i].index];
-				paths[i].number[paths[i].c] = row[paths[i].index];
-				paths[i].c++;
-				paths[i].number[paths[i].c] = -1;
-
-			}
-			else {
-			 	paths[i].sum += row[paths[i].index - 1];
-				paths[i].index = paths[i].index - 1;
-				paths[i].number[paths[i].c] = row[paths[i].index];
-				paths[i].c++;
-				paths[i].number[paths[i].c] = -1;
-			}
-		}
-	}
-}
-
-path_list init_paths(int num, int idx) {
+path_list init_path(int num) {
 	path_list p;
 	p.sum = num;
-	p.index = idx;
-	p.number[0] = num;
-	p.number[1] = -1;
-	p.active = TRUE;
-	p.c = 1;
+	p.num = num;
+	p.next = NULL;
 	return p;
 }
+void update_paths(path_list paths[], int row[], path_list new[]) {
+	int i;
+	for (i = 0; i < SIZE; i++) {
+		if (row[i] == -1) {
+			break;
+		}
+		new[i] = init_path(row[i]);
+		if (paths[i].sum > paths[i  + 1].sum) {
+			printf("Sum: %d Linking %d to %d\n", new[i].sum, new[i].num,paths[i].num);
+			new[i].next = &paths[i];
+			new[i].sum += paths[i].sum;
+		}
+		else {
+			new[i].next = &paths[i + 1];
+			new[i].sum += paths[i + 1].sum;
+			printf("Sum: %d Linking %d to %d\n", new[i].sum, new[i].num,paths[i + 1].num);
+		}
+	}
+	printf("Next!\n\n");
+}
+
 
 
 int main() {
-    /*int triangle[SIZE][SIZE] = { 
+    int triangle[SIZE][SIZE] = { 
 		{75, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 		{95, 64, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 		{17, 47, 82, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
@@ -66,35 +62,34 @@ int main() {
 		{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, -1, -1},
 		{63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, -1},
 		{ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23}
-    };*/
-	int triangle[SIZE][SIZE] = {
+    };
+	/*int triangle[SIZE][SIZE] = {
 		{3, -1, -1, -1},
 		{7, 4, -1, -1},
 		{2, 4, 6, -1},
 		{8, 5, 9, 3}
-	};
+	};*/
 
 	int i;
-	path_list paths[SIZE];
+	path_list paths[SIZE][SIZE];
 	for (i = 0; i < SIZE; i++) {
-		paths[i] = init_paths(triangle[SIZE - 1][i], i);
+		paths[0][i] = init_path(triangle[SIZE - 1][i]);
 	}
 
-	for (i = SIZE - 2; i >= 0; i--) {
-		update_paths(paths, triangle[i]);
+	for (i = 0; i <= SIZE - 2; i++) {
+		update_paths(paths[i], triangle[SIZE - 2 - i], paths[i + 1]);
+		printf("Row: %d\n", i);
 	}
 
-	int j;
+	printf("Sum: %d\n", paths[SIZE - 1][0].sum);
+	path_list p = paths[SIZE - 1][0];
 	for (i = 0; i < SIZE; i++) {
-		printf("Path: ");
-		printf("Sum: %d ", paths[i].sum);
-		for (j = 0; j < SIZE; j++) {
-			if (paths[i].number[j] == -1) {
-				break;
-			}
-			printf("-%d", paths[i].number[j]);
+		printf("Num: %d\n", p.num);
+		if (p.next == NULL) {
+			printf("Null found, leaving\n");
+			break;
 		}
-		printf("\n");
+		p = *p.next;
 	}
 }
 		
